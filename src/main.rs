3d3571a9148ff9b4d95ea3ba4181e5c834c44f9b
ibaby <utileidrissa@gmail.com>
2025-10-24 19:45:47 +0200

    removed srcs for refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-#![allow(dead_code)]
-
-use crate::node::Node;
-
-pub mod btree;
-pub mod key;
-pub mod node;
-pub mod utils;
-
-type NodePointer<K, V, const D: usize> = Option<Box<Node<K, V, D>>>;
-
 pub fn main() {}
